b2b4680ed2be4eb8c7a2e061fc4314a573e3e30a
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2024-02-26 18:05:55 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+@Input()
+photoCover:string = ""
+@Input()
+cardTitle:string = ""
+@Input()
+cardDescription:string = "O reinado da Marvel Studios conta como a empresa se livrou da falência e se tornou um dos negócios mais lucrativos do planeta. Frente ao fracasso dos filmes mais recentes, estaria esse império ameaçado?"
 }
