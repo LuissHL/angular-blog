86e7d0052c24519f5eed992e6203988d255120d6
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2024-02-26 16:57:48 -0300

    components createds with style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MenuTitleComponent,MenuBarComponent, BigCardComponent, SmallCardComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
