baca51e255953004f1946d91ee177933bdb36640
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2024-02-26 23:14:57 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
@@ -14,4 +15,7 @@ photoCover:string = ""
 cardTitle:string = ""
 @Input()
 cardDescription:string = "O reinado da Marvel Studios conta como a empresa se livrou da falência e se tornou um dos negócios mais lucrativos do planeta. Frente ao fracasso dos filmes mais recentes, estaria esse império ameaçado?"
+@Input()
+Id:string = "0"
+
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Em livro, os bastidores da Marvel. Spoiler: tem mais ação que Os Vingadores",
+    "description": "O reinado da Marvel Studios conta como a empresa se livrou da falência e se tornou um dos negócios mais lucrativos do planeta. Frente ao fracasso dos filmes mais recentes, estaria esse império ameaçado?",
+    "photoCover": "https://neofeed.com.br/wp-content/uploads/2024/02/Vingadores-Ultimato-cinema-marvel-credito-marvel.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Nova serie anunciada no Disney +",
+    "description": "ola",
+    "photoCover": "https://criticalhits.com.br/wp-content/uploads/2021/04/Upcoming-Marvel-TV-Shows-910x474.jpg"
+  }
+]
